42ad536801e9624d231529b9c56f1c3d737854c6
mpdn99 <mpdn99@gmail.com>
2020-09-05 04:38:20 +0700

    test

--- src/services/chat.js
+++ src/services/chat.js
@@ -1,7 +1,6 @@
 import database from '@react-native-firebase/database';
 
 const chat = (msg, room) => {
-  text = msg.replacingOccurrences(of: "\n", with: "\n")
   const newReference = database().ref(`/users/${room}`).push(msg);
   console.log('Auto generated key: ', newReference.key);
 };
